a8ee35254137b3125353adc541aafd2fdc12c4aa
MathSauc3 <732584@student.dusd.net>
2022-12-18 22:04:06 -0800

    Added
    Game over

--- FlappyFinalSaucedoP3/Assets/Scripts/BirdScript.cs
+++ FlappyFinalSaucedoP3/Assets/Scripts/BirdScript.cs
@@ -35,6 +35,7 @@ public class BirdScript : MonoBehaviour
         rb2d.velocity = Vector2.zero;
         isDead = true;
         anim.SetTrigger("Die");
+        GameControl.instance.BirdDied();
     }
 
 }
